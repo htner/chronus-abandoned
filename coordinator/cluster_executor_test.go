e530abb9a36a2567ba857ecbb81bce6ceb97e7ce
liliedison <liliedison@didichuxing.com>
2019-03-06 21:53:04 +0800

    add remote_executor_test.go cluster_executor_test.go

---
+++ coordinator/cluster_executor_test.go
@@ -0,0 +1,33 @@
+package coordinator_test
+
+import (
+	"testing"
+)
+
+func TestClusterExecuterTagKeys(t *testing.T) {
+}
+
+func TestClusterExecuterTagValues(t *testing.T) {
+}
+
+func TestClusterExecuterMeasurementNames(t *testing.T) {
+}
+
+func TestClusterExecuterSeriesCardinality(t *testing.T) {
+}
+
+func TestClusterExecuterFieldDimensions(t *testing.T) {
+}
+
+func TestClusterExecuterIteratorCost(t *testing.T) {
+}
+
+func TestClusterExecuterMapType(t *testing.T) {
+}
+
+func TestClusterExecuterCreateIterator(t *testing.T) {
+}
+
+func TestClusterExecuterTaskManagerStatement(t *testing.T) {
+}
+
---
+++ coordinator/remote_executor_test.go
@@ -0,0 +1,34 @@
+package coordinator_test
+
+import (
+	"testing"
+)
+
+func TestRemoteNodeExecuterTagKeys(t *testing.T) {
+}
+
+func TestRemoteNodeExecuterTagValues(t *testing.T) {
+}
+
+func TestRemoteNodeExecuterMeasurementNames(t *testing.T) {
+}
+
+func TestRemoteNodeExecuterSeriesCardinality(t *testing.T) {
+}
+
+func TestRemoteNodeExecuterFieldDimensions(t *testing.T) {
+}
+
+func TestRemoteNodeExecuterIteratorCost(t *testing.T) {
+}
+
+func TestRemoteNodeExecuterMapType(t *testing.T) {
+}
+
+func TestRemoteNodeExecuterCreateIterator(t *testing.T) {
+}
+
+func TestRemoteNodeExecuterTaskManagerStatement(t *testing.T) {
+}
+
+
